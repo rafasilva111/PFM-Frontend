0cddafd4cba175536cc640d1d1a93d9cfe673453
rafasilva111 <rafael.francisco111@gmail.com>
2022-11-17 23:01:12 +0000

    main menu

--- app/src/main/java/com/example/projectfoodmanager/MainActivity.kt
+++ app/src/main/java/com/example/projectfoodmanager/MainActivity.kt
@@ -1,13 +1,31 @@
 package com.example.projectfoodmanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
+import com.example.projectfoodmanager.views.ReceitasViewActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_menu)
+        //TODO
+
+        //check internet
+
+
+        //send login
+
+        val openRecipes = findViewById<Button>(R.id.button2)
+        openRecipes.setOnClickListener {
+            startActivity(Intent(this,ReceitasViewActivity::class.java))
+        }
+
+
     }
+
 }
