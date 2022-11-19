586f1cb8efa76608dfb6f96211b071b2963121d0
rafasilva111 <rafael.francisco111@gmail.com>
2022-11-19 16:03:05 +0000

    tool bar menu, o produto final pode nãoficar assim mas parece me mais clean asssim

--- app/src/main/java/com/example/projectfoodmanager/MainActivity.kt
+++ app/src/main/java/com/example/projectfoodmanager/MainActivity.kt
@@ -3,16 +3,44 @@ package com.example.projectfoodmanager
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.replace
+import com.example.projectfoodmanager.databinding.ActivityTestMainMenuBinding
+import com.example.projectfoodmanager.databinding.FragmentRecipeListingBinding
+import com.example.projectfoodmanager.recipe.RecipeListingFragment
+import com.example.projectfoodmanager.views.*
 
-import com.example.projectfoodmanager.views.ReceitasViewActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityTestMainMenuBinding
+    val TAG: String = "ReceitaListingFragment"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_menu)
+
+        binding = ActivityTestMainMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(RecipeListingFragment())
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when (it.itemId) {
+
+                R.id.recipes ->replaceFragment(RecipeListingFragment())
+                R.id.calender ->replaceFragment(CalenderFragment())
+                R.id.favorites ->replaceFragment(FavoritesFragment())
+                R.id.goal ->replaceFragment(GoalFragment())
+                R.id.profile ->replaceFragment(ProfileFragment())
+                else -> {
+                    Log.d(TAG, "onCreate: ")
+                }
+            }
+            true
+        }
         //TODO
 
         //check internet
@@ -20,12 +48,15 @@ class MainActivity : AppCompatActivity() {
 
         //send login
 
-        val openRecipes = findViewById<Button>(R.id.button2)
-        openRecipes.setOnClickListener {
-            startActivity(Intent(this,ReceitasViewActivity::class.java))
-        }
+
 
 
     }
 
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
+    }
 }
